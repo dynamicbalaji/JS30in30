cd7164dbefec6f653882c3e69cb0e783d5234250
dynamicbalaji <balajiashokkumar@yahoo.com>
2019-02-09 15:58:21 +0530

    Adding function to handle volume & playbackspeed range selector

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -27,9 +27,16 @@ function skip() {
     video.currentTime += parseFloat(this.dataset.skip);
 }
 
+function handleRangeChange(){
+    console.log('this.name: ', this.name);
+    console.log('this.value: ', this.value);
+    video[this.name] = this.value;
+}
+
 /* Hook up the event listeners */
 video.addEventListener('click', togglePlay);
 video.addEventListener('pause', updateButton);
 video.addEventListener('play', updateButton);
 toggle.addEventListener('click', togglePlay);
 skipButtons.forEach(button => button.addEventListener('click', skip));
+ranges.forEach(range => range.addEventListener('click', handleRangeChange));
